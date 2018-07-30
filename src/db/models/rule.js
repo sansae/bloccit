5f51aa7f615b6cafa12102f10f32f011288be850
Kent <kentsaeteurn@gmail.com>
2018-07-30 08:38:43 -0700

    create Rule model with description property

---
+++ src/db/models/rule.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Rule = sequelize.define('Rule', {
+    description: DataTypes.STRING
+  }, {});
+  Rule.associate = function(models) {
+    // associations can be defined here
+    Rule.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+  };
+  return Rule;
+};
