12418dc29d9b672e39f3900a068b2d9e127a2d3e
Kent <kentsaeteurn@gmail.com>
2018-08-28 15:25:10 -0700

    authorize crud operations for admin and users: fix errors

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -12,6 +12,10 @@ module.exports = (sequelize, DataTypes) => {
     topicId: {
       type: DataTypes.INTEGER,
       allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   }, {});
   Post.associate = function(models) {
@@ -19,7 +23,12 @@ module.exports = (sequelize, DataTypes) => {
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
       onDelete: "CASCADE",
-    })
+    });
+
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE"
+    });
   };
   return Post;
 };
