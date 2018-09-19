ae3d4c6b195016adae5f63c7c5707f76b4d1a763
Kent <kentsaeteurn@gmail.com>
2018-09-19 07:16:06 -0700

    implement the favorite resource

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -40,6 +40,18 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "votes",
     });
+
+    Post.hasMany(models.Favorite, {
+      foreignKey: "postId",
+      as: "favorites"
+    });
+
+    Post.afterCreate((post, callback) => {
+      return models.Favorite.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
 
   Post.prototype.getPoints = function() {
@@ -71,6 +83,10 @@ module.exports = (sequelize, DataTypes) => {
         value: -1
       }
     })
+  };
+
+  Post.prototype.getFavoriteFor = function(userId) {
+    return this.favorites.find((favorite) => { return favorite.userId == userId });
   };
 
   return Post;
