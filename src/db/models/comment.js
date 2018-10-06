501c27c8868ed5d34804947337beb5e54037bd56
Kent <kentsaeteurn@gmail.com>
2018-10-06 12:13:58 -0700

    fix comment to post association

--- src/db/models/comment.js
+++ src/db/models/comment.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
     Comment.belongsTo(models.Post, {
       foreignKey: "postId",
-      allowNull: false,
+      onDelete: "CASCADE",
     });
 
     Comment.belongsTo(models.User, {
